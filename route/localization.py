c85a04c470373bb52f15aaf3a5068102893ce454
Sergei <kolesniksn2510@gmail.com>
2023-04-22 16:33:17 +0300

    Add translation for arb file

--- route/localization.py
+++ route/localization.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 
 from data.json_request import ArbJsonRequest
 from translation.translation_func import translate_dict_values
+from utils.file import get_dict_from_arb_file
 
 router = APIRouter(prefix="/localization", tags=["Localization"])
 
@@ -11,3 +12,11 @@ def local_from_json(data: ArbJsonRequest, source_lang: str = "en", target_lang="
     translate_dict_values(source_lang, target_lang, data.payload)
 
     return data.payload
+
+
+@router.post("/local-from-file")
+def local_from_file(data: UploadFile, source_lang: str = "en", target_lang="ru") -> dict:
+    data_dict = get_dict_from_arb_file(data.file)
+    translate_dict_values(source_lang, target_lang, data_dict)
+
+    return data_dict
