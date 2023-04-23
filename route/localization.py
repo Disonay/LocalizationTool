9f31000d6f0d1b29a2f617981d456444941639c5
Sergei <kolesniksn2510@gmail.com>
2023-04-23 21:01:26 +0300

    Fix endpoints and add docstrings

--- route/localization.py
+++ route/localization.py
@@ -1,22 +1,42 @@
-from fastapi import APIRouter, UploadFile
+import json
+import tempfile
 
-from data.json_request import ArbJsonRequest
+from fastapi import APIRouter, UploadFile, Body
+from fastapi.responses import FileResponse
+
 from translation.translation_func import translate_dict_values
 from utils.file import get_dict_from_arb_file
 
-router = APIRouter(prefix="/localization", tags=["Localization"])
+router = APIRouter(prefix="/localization", tags=["localization"])
 
 
 @router.post("/local-from-json")
-def local_from_json(data: ArbJsonRequest, source_lang: str = "en", target_lang="ru") -> dict:
-    translate_dict_values(source_lang, target_lang, data.payload)
+async def local_from_json(data: dict = Body(...), source_lang: str = "en", target_lang="ru") -> dict:
+    """
+    Translates the arb file sent in json format
 
-    return data.payload
+    - **data**: arb file in json format
+    - **source_lang**: original language
+    - **target_lang**: language after translation
+    """
+    translate_dict_values(source_lang, target_lang, data)
+
+    return data
 
 
 @router.post("/local-from-file")
-def local_from_file(data: UploadFile, source_lang: str = "en", target_lang="ru") -> dict:
+async def local_from_file(data: UploadFile, source_lang: str = "en", target_lang="ru") -> FileResponse:
+    """
+    Translates the arb file
+
+    - **data**: arb file
+    - **source_lang**: original language
+    - **target_lang**: language after translation
+    """
     data_dict = get_dict_from_arb_file(data.file)
     translate_dict_values(source_lang, target_lang, data_dict)
 
-    return data_dict
+    with tempfile.NamedTemporaryFile(suffix=".arb", delete=False, mode="w", encoding="utf-8") as temp_file:
+        json.dump(data_dict, temp_file, indent=2, ensure_ascii=False)
+
+        return FileResponse(temp_file.name, media_type="application/json", filename=target_lang + ".arb")
